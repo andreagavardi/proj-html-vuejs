e27deda82e4083349615fbafaf3276ef590cfc8a
andreagavardi <andreagavardi89@gmail.com>
2021-06-15 13:06:02 +0200

    initial commit e navbar

---
+++ src/app.js
@@ -0,0 +1,36 @@
+
+
+ const app = new Vue ({ 
+    el: '#app',
+    data:{
+
+        navbarLinks:[
+            {
+                linkName:'home',
+                link:'#'
+            },
+             {
+                linkName:'about',
+                link:'#'
+            },
+             {
+                linkName:'services',
+                link:'#'
+            },
+             {
+                linkName:'work',
+                link:'#'
+            },
+             {
+                linkName:'articles',
+                link:'#'
+            },
+             {
+                linkName:'get quote',
+                link:'#'
+            }
+        ]
+
+     },
+    methods:{ } 
+}) 
---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+let mix = require('laravel-mix');
+
+mix
+.js('src/app.js', 'js')
+.sass('src/app.scss','css')
+.options({
+  processCssUrls: false // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
+})
+.setPublicPath('dist');
