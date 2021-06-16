8852e45e1def3e04c7a7fd5a2608e9ea50d4180b
andreagavardi <andreagavardi89@gmail.com>
2021-06-16 12:41:55 +0200

    terminata sezione statistics

--- dist/js/app.js
+++ dist/js/app.js
@@ -33,6 +33,27 @@ var app = new Vue({
       linkName: 'get quote',
       link: '#',
       selected: false
+    }],
+    statistics: [{
+      icon: "fas",
+      iconType: "fa-suitcase",
+      title: "3534",
+      description: "planning applications"
+    }, {
+      icon: "far",
+      iconType: "fa-building",
+      title: "896",
+      description: "completed projects"
+    }, {
+      icon: "fas",
+      iconType: "fa-users",
+      title: "172",
+      description: "trained professionals"
+    }, {
+      icon: "fas",
+      iconType: "fa-globe",
+      title: "19",
+      description: "internationals offices"
     }]
   },
   methods: {}
--- src/app.js
+++ src/app.js
@@ -34,6 +34,33 @@
                 link:'#',
                 selected:false
             }
+        ],
+
+        statistics:[
+            {
+                icon:"fas",
+                iconType:"fa-suitcase",
+                title:"3534",
+                description:"planning applications"
+            },
+             {
+                icon:"far",
+                iconType:"fa-building",
+                title:"896",
+                description:"completed projects"
+            },
+             {
+                icon:"fas",
+                iconType:"fa-users",
+                title:"172",
+                description:"trained professionals"
+            },
+             {
+                icon:"fas",
+                iconType:"fa-globe",
+                title:"19",
+                description:"internationals offices"
+            }
         ]
 
      },
